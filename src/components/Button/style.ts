f3027feda0a25364a389f630fc0de529d037da87
cohaviyarden <96189478+cohaviyarden@users.noreply.github.com>
2021-12-23 15:12:56 +0200

    Feature/button (#1)
    * done feature button

--- src/components/Button/style.ts
+++ src/components/Button/style.ts
@@ -1,22 +1,38 @@
 import styled from "styled-components";
+import { colors } from "../../variables";
+import { ButtonProps } from "./Button";
 
-export const Btn = styled.button`
+export const Btn = styled.button<ButtonProps>`
     height: 36px;
-    width: 226px;
+    width: ${props => ((props.size === 'small') ? `157px` : ((props.size === 'large') ? `714px` : '226px'))};
     border-radius: 20px;
-    padding: 0.5rem 1.5rem;
-    color: white;
-    background: #3379C7;
+    background: ${props => (props.variant === 'secondary' ? colors.secondary_grey : colors.primary_blue)};
     cursor: pointer;
+    display:flex;
+    justify-content: center;
+    align-items:center;
     border: none;
-
-    &:hover,
-    &:active {
-        background: #3379C7;
+    
+    &:hover{
+        background: ${props => (props.variant === 'secondary' ? colors.secondary_grey_hover : colors.primary_blue_hover)};
     }
 `;
+export const Label = styled.p`
+@font-face {
+    font-family: "Roboto";
+    src: url('../../assets/fonts/Roboto-Light.ttf') ;
+}
+    text-align: center;
+    white-space: pre;
+    text-transform: uppercase;
+    color: white;
+    font-size: 14px;
+    letter-spacing: 0.25px;
+`
 export const Icon = styled.img`
-    fill: white;
-    border: 1.5px solid #FFFFFF;
+    width:30px;
+    height:20px;
     transform: rotateY(180deg);
+    filter:  invert(100%) sepia(13%) saturate(7488%) hue-rotate(182deg) brightness(121%) contrast(94%);
+]
 `
