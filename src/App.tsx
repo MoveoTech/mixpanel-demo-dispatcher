450d6093aea23e8e31e928ef7fb41923efc27ee9
Yarden Cohavi <yarden@moveohls.com>
2021-12-28 22:40:11 +0200

    changes update after PR Navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
-import React from "react";
+
 import "./App.css";
-import Button from "./components/Button/Button";
-import Navbar from "./components/Navbar/Navbar";
-import { SIZE_TYPE, VARIANT } from "./types";
-import icon from './assets/icons/back.svg';
-import Search from "./components/Search/Search";
 
 function App() {
   return(
     <div>
-      <Navbar>YC</Navbar>
-      {/* <Button onClick={() => window.open("https://www.ynet.co.il/", "_blank")} size={SIZE_TYPE.small} variant={VARIANT.primary} icon={icon}>continue</Button> */}
     </div>
   )
 }
