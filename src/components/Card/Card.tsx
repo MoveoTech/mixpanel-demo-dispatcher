f4f7911d6cf716a8e602db6021e22ce177b28e68
Yarden Cohavi <yarden@moveohls.com>
2022-01-02 10:06:02 +0200

    prettier

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,13 +1,24 @@
-
-import { useMediaQuery } from 'react-responsive';
-import { convertDate, renderTags } from '../../utils/utils';
-import { ButtonProps } from '../Button/Button';
-import { BodyCard, CardBtn, CardStyled, ImageCard, Description, Title, TagsContainer, Tag, Row, SourceCard, DateCard } from './style';
-import { device } from '../../globalStyle';
+import { useMediaQuery } from "react-responsive";
+import { convertDate, renderTags } from "../../utils/utils";
+import { ButtonProps } from "../Button/Button";
+import {
+  BodyCard,
+  CardBtn,
+  CardStyled,
+  ImageCard,
+  Description,
+  Title,
+  TagsContainer,
+  Tag,
+  Row,
+  SourceCard,
+  DateCard,
+} from "./style";
+import { device } from "../../globalStyle";
 
 export interface Source {
-  id: string,
-  name: string
+  id: string;
+  name: string;
 }
 export interface CardProps {
   date: string;
@@ -21,29 +32,27 @@ export interface CardProps {
 
 const Card = (props: CardProps) => {
   const isMobileDevice = useMediaQuery({
-    query: device.mobile
-  })
-    return (
-      <CardStyled>
-          <ImageCard src={props.image} />
-          <BodyCard>
-            <Row>
-              <DateCard>{convertDate(props.date)}</DateCard>
-              <TagsContainer>
-                  {
-                    renderTags(props.tags, isMobileDevice).map((tag,index) => {
-                      return <Tag key={index}>{tag}</Tag>
-                    })
-                   }
-              </TagsContainer>
-            </Row> 
-            <Title>{props.title}</Title>
-            <SourceCard>{props.source.name}</SourceCard>
-            <Description>{props.description}</Description>
-            <CardBtn {...props.button}/>
-          </BodyCard>
-      </CardStyled>
-    );
-  };
-  
-  export default Card;
+    query: device.mobile,
+  });
+  return (
+    <CardStyled>
+      <ImageCard src={props.image} />
+      <BodyCard>
+        <Row>
+          <DateCard>{convertDate(props.date)}</DateCard>
+          <TagsContainer>
+            {renderTags(props.tags, isMobileDevice).map((tag, index) => {
+              return <Tag key={index}>{tag}</Tag>;
+            })}
+          </TagsContainer>
+        </Row>
+        <Title>{props.title}</Title>
+        <SourceCard>{props.source.name}</SourceCard>
+        <Description>{props.description}</Description>
+        <CardBtn {...props.button} />
+      </BodyCard>
+    </CardStyled>
+  );
+};
+
+export default Card;
